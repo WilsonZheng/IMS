473aae076f7a9fa9c67f787347544f7c768d1574
Wilson Zheng <zhwzhw29@gmail.com>
2016-06-20 10:31:27 +1200

    Created project

---
+++ IMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IMS.Startup))]
+namespace IMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
