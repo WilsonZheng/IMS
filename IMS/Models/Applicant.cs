ef090f7a2912f29f4f048eb3319907090554f081
Wilson Zheng <zhwzhw29@gmail.com>
2016-06-27 11:58:02 +1200

    updated applicant.cs

--- IMS/Models/Applicant.cs
+++ IMS/Models/Applicant.cs
@@ -8,5 +8,16 @@ namespace IMS.Models
     public class Applicant
     {
         public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string VisaStatus { get; set; }
+        public string MedicalCondition { get; set; }
+        public string Company { get; set; }
+        public string InvitationCode { get; set; }
+        public int SignatureId { get; set; }
+        public int MediaId { get; set; }
     }
 }
