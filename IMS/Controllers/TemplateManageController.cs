d174446ef2725e39fe6a9689c1dced121996e132
jayinsight <jayinsight@users.noreply.github.com>
2016-06-28 11:40:29 +1200

    Add controller

---
+++ IMS/Controllers/TemplateManageController.cs
@@ -0,0 +1,44 @@
+﻿using IMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Controllers
+{
+    public class TemplateManageController : Controller
+    {
+        // GET: TemplateManage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Email() {
+         
+                using (var db = new ApplicationDbContext())
+                {
+                    var result = db.Templates.Where(x => x.TemplateType.Code.Equals("ET")).ToList();
+                    return View(result);
+                }         
+           
+        }
+
+        public ActionResult NewEmail() {
+            return View();
+        }
+
+        public ActionResult Contract() {
+            return View();
+        }
+
+
+
+       
+
+
+      
+
+    }
+}
