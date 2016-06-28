3ae49ad792110a3e68d733f1260f41d08537170c
Wilson Zheng <zhwzhw29@gmail.com>
2016-06-28 16:39:38 +1200

    testing code for contract

--- IMS/Controllers/RegistrationController.cs
+++ IMS/Controllers/RegistrationController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using IMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,28 @@ namespace IMS.Controllers
         // GET: Contract
         public ActionResult Index()
         {
+            var m = new
+            {
+                Name = "John Henderson",
+                KiwiName = "johnny",
+                Address = "480 Queens St",
+                Suburb = "Auckland",
+                Email = "john@gmail.com",
+                Mobile = "021123456",
+                VisaStatus = "Citizen",
+                MedicalCondition = "None",
+                Signature = "signed here",
+                Date = "2/5/2016"
+            };
+            using(var db= new ApplicationDbContext())
+            {
+                var template = db.Templates.Where(x=> x.Id==2).Single();
+                var content = Encoding.UTF8.GetString(template.Content);
+                var html=IMS.Media.DocGenerator.Html(content, m);
+                IMS.Media.DocGenerator.Pdf(html, @"D:\test.pdf");
+            }
+            
+            
             return View();
         }
     }
