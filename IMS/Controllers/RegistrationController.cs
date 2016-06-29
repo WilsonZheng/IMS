70005a55ed9839fa9f1c8807fae97184fa37e61c
Wilson Zheng <zhwzhw29@gmail.com>
2016-06-29 16:18:37 +1200

    update register page of applicant

--- IMS/Controllers/RegistrationController.cs
+++ IMS/Controllers/RegistrationController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
-
+using IMS.ViewModels;
 namespace IMS.Controllers
 {
     public class RegistrationController : Controller
@@ -13,29 +13,44 @@ namespace IMS.Controllers
         // GET: Contract
         public ActionResult Index()
         {
-            var m = new
+            ApplicantInfo model = new ApplicantInfo();     
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddApplicant(ApplicantInfo model)
+        {
+
+            using (var db = new ApplicationDbContext())
             {
-                Name = "John Henderson",
-                KiwiName = "johnny",
-                Address = "480 Queens St",
-                Suburb = "Auckland",
-                Email = "john@gmail.com",
-                Mobile = "021123456",
-                VisaStatus = "Citizen",
-                MedicalCondition = "None",
-                Signature = "signed here",
-                Date = "2/5/2016"
-            };
-            using(var db= new ApplicationDbContext())
-            {
-                var template = db.Templates.Where(x=> x.Id==2).Single();
-                var content = Encoding.UTF8.GetString(template.Content);
-                var html=IMS.Media.DocGenerator.Html(content, m);
-                IMS.Media.DocGenerator.Pdf(html, @"D:\test.pdf");
+                db.Applicants.Add(new Applicant
+                {
+                    Firstname = model.firstname,
+                    Lastname = model.lastname,
+                    Mobile = model.mobile,
+                    MedicalCondition = model.medicalCondition,
+                    Address = model.address + ", " + model.suburb,
+                    Email = model.email,
+                    VisaStatus = model.visaStatus,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    
+                    
+
+                });
+                db.SaveChanges();
+                return View(model);
             }
+
+
+            
+               
             
             
-            return View();
+            
+            
         }
     }
+    
 }
---
+++ IMS/ViewModels/ApplicantInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMS.ViewModels
+{
+    public class ApplicantInfo
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string kiwiname { get; set; }
+        public string email { get; set; }
+        public string mobile { get; set; }
+        public string address { get; set; }
+        public string suburb { get; set; }
+        public string visaStatus { get; set; }
+        public string medicalCondition { get; set; }
+
+
+    }
+}
