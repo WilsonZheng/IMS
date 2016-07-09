204a0cde4ce4cc6b7c84194c3a630c58b002de59
jayinsight <jayinsight@users.noreply.github.com>
2016-07-09 20:36:32 +1200

    PrimeNG sample - Editor, InputText  & 2 Way binding.

---
+++ IMS/app/sample/editor/app.ts
@@ -0,0 +1,21 @@
+﻿import {Component, Injectable} from '@angular/core';
+import {InputText, Editor, Header} from 'primeng/primeng';
+import { MyMessage} from './message.comp';
+import {MyLogger} from './mylogger.svc';
+@Component({
+    selector: 'my-app',
+    template:
+    `
+        <input type="text" pInputText [(ngModel)]="Address"/>
+        <p-editor [(ngModel)]="Address" [style]="{'height':'320px'}"></p-editor>
+        <p>
+            <my-message></my-message>
+        </p>
+    `,
+    directives: [MyMessage, InputText, Editor, Header],
+    providers: [MyLogger]
+})
+@Injectable()
+export class AppComponent {
+    Address = "aaaaaaaaaaaaaaaaaaaaaaaaaaGod!";
+}
---
+++ IMS/app/sample/editor/message.comp.ts
@@ -0,0 +1,15 @@
+﻿import {Component, Injectable} from '@angular/core';
+import {MyLogger} from './mylogger.svc'
+@Component({
+    selector: 'my-message',
+    template:
+    `  Here is a message for you{{text}}.
+    `
+})
+@Injectable()
+export class MyMessage{
+    public text: string;
+    constructor(loger: MyLogger) {
+        this.text = loger.name;
+    }
+}
