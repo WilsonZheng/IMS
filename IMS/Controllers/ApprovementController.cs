e20c7f19dc7d9aff7682b31cc74a45d9ef541956
Wilson Zheng <zhwzhw29@gmail.com>
2016-08-06 12:34:38 +1200

    Added Approvment Controller

--- IMS/Controllers/ApprovementController.cs
+++ IMS/Controllers/ApprovementController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using IMS.Common;
+using IMS.Models;
+using IMS.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace IMS.Controllers
 {
@@ -14,5 +18,36 @@ namespace IMS.Controllers
         {
             return View("");
         }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult GetList(jQueryDataTableParamModel param)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                //IEnumerable<DisplayApplicantsViewModel> applicants;
+                var applicants = db.Applicants.Where(
+                    x => x.IsActive == true &&
+                    x.Invitation.RecruitStatusType.Code == (int)RecruitStatusCode.ContractReceived)
+                    .Select(x => new DisplayApplicantsViewModel()
+                    {
+
+                        Firstname = x.Firstname,
+                        Lastname = x.Lastname,
+                        TemplateId = x.TemplateId.ToString(),
+                        Email = x.Email,
+                        Mobile = x.Mobile,
+                        VisaStatus = x.VisaStatus,
+                        MedicalCondition = x.MedicalCondition,
+                        //Invitation = x.Invitation,
+                    }).ToList();
+
+                //List<DisplayApplicantsViewModel> result = applicants.ToList();
+                var json = new JavaScriptSerializer().Serialize(applicants);
+                return Json(json, JsonRequestBehavior.AllowGet);
+            }
+
+
+        }
+
     }
 }
